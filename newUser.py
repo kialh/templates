4e59aaf27f27f39eaff83a0bfb459683e0d52d76
kia2lh <kia2lh@gmail.com>
2017-08-04 14:50:46 +0200

    Second commit

---
+++ newUser.py
@@ -0,0 +1,7 @@
+tnum = input("Introduce the ticket number (without TMC#): ")
+desc = input("Enter the CR title: ")
+final = '{0}'.format(
+"1) Log into "
+    "the FortiManager with your windows credentials\n\
+2) Lock the ADOM called EUROPE \n\
+)
